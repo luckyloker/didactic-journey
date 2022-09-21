16aaac10efa2b014eb2d46564f160a36161ba4a3
Serhii Bondarenko <serhii.s.bondarenko@gmail.com>
2022-09-21 19:40:16 +0300

    [C-8] New endpoint

--- src/main/java/com/example/controller/SimpleController.java
+++ src/main/java/com/example/controller/SimpleController.java
@@ -1,6 +1,7 @@
 package com.example.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,6 +15,11 @@ public class SimpleController {
     @GetMapping("/whothebest")
     public String whothebest() {
         return "Tanya - THE BEST!";
+    }
+
+    @GetMapping("/greetings")
+    public String greeting(@RequestParam String name) {
+        return String.format("Greetings from cloud, %s", name);
     }
 
 }
